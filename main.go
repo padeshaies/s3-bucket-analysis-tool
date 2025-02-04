e43db614d6599b7821f3182607ecc88a95d42e73
PA Deshaies <pierreantoine.deshaies@gmail.com>
2025-02-04 14:41:05 -0500

    Basic listing of S3 buckets

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Buckets:")
+	for _, bucket := range output.Buckets {
+		fmt.Println(*bucket.Name)
+	}
 }
